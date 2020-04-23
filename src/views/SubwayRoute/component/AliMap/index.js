24b6a72248ae71fe2a783a3216a5715de5d8a0bf
zhanshiyu <zhanshiyu@jd.com>
2020-04-23 21:00:19 +0800

    update map

--- src/views/SubwayRoute/component/AliMap/index.js
+++ src/views/SubwayRoute/component/AliMap/index.js
@@ -2,16 +2,118 @@
  * 高德地图
  */
 import React, { Component } from 'react';
+import { Map, Marker } from 'react-amap';
+import { Input } from 'antd';
 import './index.css';
 
 class AliMap extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            routesStart: '', // 起始点
+            routesEnd: '', // 终点
+            routesName: '', // 路线名称
+            // 设置坐标点，就会在地图上显示一个 标记点
+            markerPosition: { longitude: 120, latitude: 35 },
+        }
+        // 高德地图 Marker 实例
+        this.markerInstance = undefined
+        // 高德地图 Map 实例
+        this.mapInstance = undefined
+        this.amapEvents = {
+            created: mapInstance => {
+                console.log('高德地图 Map 实例创建成功；如果你要亲自对实例进行操作，可以从这里开始。比如：');
+                console.log('缩放级别：', mapInstance.getZoom());
+                this.mapInstance = mapInstance
+              
+                //eslint-disable-next-line
+                AMap.plugin(['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.CitySearch'], () => {
+                    // 实例化Autocomplete
+                    const autoOptions = {
+                        // city 限定城市，默认全国
+                        // city: '025',
+                        // input 为绑定输入提示功能的input的DOM ID
+                        input: 'amapInput',
+                    }
+                    //eslint-disable-next-line
+                    const autoComplete = new AMap.Autocomplete(autoOptions);
+                    // 无需再手动执行search方法，autoComplete会根据传入input对应的DOM动态触发search
+      
+                    //eslint-disable-next-line
+                    const placeSearch = new AMap.PlaceSearch({
+                        // city: '南京',
+                        map: mapInstance,
+                    });
+      
+                    // 监听下拉框选中事件
+                    //eslint-disable-next-line
+                    AMap.event.addListener(autoComplete, 'select', e => {
+                        // TODO 针对选中的poi实现自己的功能
+                        placeSearch.setCity(e.poi.adcode)
+                        placeSearch.search(e.poi.name)
+                    });
+      
+      
+                    //eslint-disable-next-line
+                    const citySearch = new AMap.CitySearch()
+                    citySearch.getLocalCity((status, result) => {
+                        if (status === 'complete' && result.info === 'OK') {
+                            // 查询成功，result即为当前所在城市信息
+                            console.log('当前所在城市：', result)
+                        if (result && result.city && result.bounds) {
+                            // 当前城市名称
+                            // const cityinfo = result.city;
+      
+                            // 当前城市位置信息
+                            const citybounds = result.bounds;
+                            // document.getElementById('info').innerHTML = '您当前所在城市：'+cityinfo;
+                            // 地图显示当前城市
+                            mapInstance.setBounds(citybounds);
+                            // 需要在设置坐标成功后，重新设置 缩放级别
+                            // mapInstance.setZoom(15)
+                        }
+                    }
+                });
+            });
+                // 实例点击事件
+                    mapInstance.on('click', e => {
+                    const lngLat = `${e.lnglat.getLat()},${e.lnglat.getLng()}`
+                    console.log('坐标位置:', lngLat)
+                    this.props.onChange(lngLat)
+                });
+            },
+        };
+        this.markerEvents = {
+            created: markerInstance => {
+                console.log('高德地图 Marker 实例创建成功；如果你要亲自对实例进行操作，可以从这里开始。比如：');
+                console.log(markerInstance.getPosition());
+      
+                this.markerInstance = markerInstance
+            },
+        }
+        // this.markerPosition = { longitude: 120, latitude: 30 };
     }
 
+    componentDidUpdate(prevProps) {
+        const { value } = this.props
+        if (this.props.value !== prevProps.value) {
+            if (value) {
+                const temp = value.split(',')
+    
+                // 重新设置地图坐标点
+                this.setState({ markerPosition: { longitude: temp[1], latitude: temp[0] } }, () => {
+                    // 需要在设置坐标成功后，重新设置 缩放级别
+                    if (this.mapInstance) {
+                        this.mapInstance.setZoom(15)
+                    }
+                });
+            }
+        }
+    }
+    
+
     componentDidMount() {
-        this.aliMap();
+        // this.aliMap();
     }
 
     easyMap = () => {
@@ -114,15 +216,46 @@ class AliMap extends Component {
 
         // 工具条
         //eslint-disable-next-line
-        let toolbar = new AMap.ToolBar();
+        let toolbar = new AMap.ToolBar();  
+    }
+
+    handleStartChange = e => {
+        this.setState({
+            routesStart: e.target.value
+        });
+    }
+
+    handleEndChange = e => {
+        this.setState({
+            routesEnd: e.target.value
+        });
     }
 
     render() {
+        const { routesStart, routesEnd, routesName } = this.state;
         return(
-            <div className='ali-map' style={{width:'100%',height:'500px'}}>
-                <div id='mysubway' style={{width:'100%',height:'100%'}}></div>
-                <div></div>
-            </div>
+            // <div className='ali-map' style={{width:'100%',height:'500px'}}>
+            //     <div id='mysubway' style={{width:'100%',height:'100%'}}></div>
+            //     <div></div>
+            // </div>
+            <>
+                <div className='map' style={{ width: '100%', height: '450px', position: 'relative' }}>
+                    {/* zoom={15} 设置后，无效，不知道什么原因，必须手动设置 */}
+                    <Map plugins={['ToolBar']} events={this.amapEvents} amapkey={'d22d7df460d244873addaddd4117ef6e'} center={this.state.markerPosition}>
+                        <Marker position={this.state.markerPosition} events={this.markerEvents} />
+                    </Map>
+                    <div className='map-position'>
+                        <div className='map-start'>
+                            <div className='map-start-text'>起始点</div>
+                            <Input id='amapInput' className='map-start-input' value={routesStart} onChange={this.handleStartChange}></Input>
+                        </div>
+                        <div className='map-end'>
+                            <div className='map-end-text'>终点</div>
+                            <Input id='amapInput' className='map-end-input' value={routesEnd} onChange={this.handleEndChange}></Input>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
