722f3312143c32ac62ca9e3d8188e5b48331203f
zhanshiyu <zhanshiyu@jd.com>
2020-04-29 17:46:00 +0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy("/zsy/api/", {
+    proxy("/api", {
       target: "http://localhost:8001",
       changeOrigin: true
     })
